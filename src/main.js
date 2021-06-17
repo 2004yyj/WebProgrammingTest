6224609a038d4f128343dca64b69e363e0bbf4f4
양윤재 <diddbswo04@dgsw.hs.kr>
2021-06-17 10:16:08 +0900

    [Update] Vuetify 추가

--- src/main.js
+++ src/main.js
@@ -2,10 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuetify from 'vuetify/lib'
+import vuetify from './plugins/vuetify'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
+Vue.use(Vuetify);
 
 new Vue({
+  icons: {
+    iconfont: 'mdi', // default - only for display purposes
+  },
+  vuetify,
   router,
   store,
   render: h => h(App)
